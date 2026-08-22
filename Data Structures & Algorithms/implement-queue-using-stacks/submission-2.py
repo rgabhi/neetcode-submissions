13985deb9d1f81c9b8b9d4bf49f61c43d3890daa
Abhinav Gupta <45824234+rgabhi@users.noreply.github.com>
2026-08-22 17:20:13 +0530

    Add: implement-queue-using-stacks - submission-2

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-2.py
@@ -0,0 +1,36 @@
+class MyQueue:
+
+    def __init__(self):
+        self.st1 = []
+        self.st2 = []
+        # [1, 2, 3, 4, 5]
+        # [5, 4, 3, 2]
+    def push(self, x: int) -> None:
+        self.st1.append(x)
+        
+    def pop(self) -> int:
+        if self.st2:
+            return self.st2.pop()
+        while self.st1:
+            self.st2.append(self.st1.pop())
+        return self.st2.pop()
+
+    def peek(self) -> int:
+        if self.st2:
+            return self.st2[-1]
+        while self.st1:
+            self.st2.append(self.st1.pop())
+        return self.st2[-1]
+        
+
+    def empty(self) -> bool:
+        return len(self.st1) + len(self.st2) == 0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
